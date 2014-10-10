ef357ec82ed9a89409eed45312512aad133e1fb3
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2014-10-10 17:42:57 +0200

    Moved depth limiting logic to queue class

--- search.py
+++ search.py
@@ -16,28 +16,27 @@ def dfs(problem):
 
 def bfs(problem):
 	return general_search(problem, queue_multi_insert(queue.Queue)())
-	
-def dls(problem, queue, depth):
-	queue.put(adt.Node(problem.initial_state, root=True))
-	while not queue.empty():
-	  node = queue.get()
-	  if node.depth > depth:
-	  	continue
-	  if problem.goal_test(node.state):
-	    return node
-	  queue.put_many(node.expand(problem.operators))
-	  pass
-	return None	
 
 def ids(problem):
 	i = 0
 	while True:
-		node = dls(problem, queue_multi_insert(queue.LifoQueue)(), i)
+		node = general_search(problem, LimitedDepthQueue(i))
 		if node != None:
 			return node
 		i += 1
 
+@queue_multi_insert
+class LimitedDepthQueue(queue.LifoQueue):
+  def __init__(self, depth, *args, **kwargs):
+    self.depth = depth
+    super().__init__(*args, **kwargs)
+
+  def put(self, node, *args, **kwargs):
+    if node.depth > self.depth:
+      return
+    super().put(node, *args, **kwargs)
+
 if __name__ == "__main__":
   import ttfe, sys
   p = ttfe.TTFE(int(sys.argv[1]))
-  print(dfs(p))
+  print(ids(p))
