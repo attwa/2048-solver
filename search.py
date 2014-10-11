d3ce6a6e0294dadc3172aadbb65ee30c4941f291
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2014-10-11 21:35:49 +0200

    search methods now return number of expanded nodes

--- search.py
+++ search.py
@@ -2,14 +2,18 @@ import queue, adt
 from decorators import queue_multi_insert
 
 def general_search(problem, queue):
+  num_expanded = 0
   queue.put(adt.Node(problem.initial_state, root=True))
   while not queue.empty():
     node = queue.get()
+    num_expanded += 1
+    node.expanded = num_expanded
+    #print(node, problem.heuristics[0](node.state))
     if problem.goal_test(node.state):
-      return node
+      return node, num_expanded
     queue.put_many(node.expand(problem.operators))
     pass
-  return None
+  return None, num_expanded
 
 def dfs(problem):
   return general_search(problem, queue_multi_insert(queue.LifoQueue)())
@@ -22,11 +26,13 @@ def greedy(problem, heuristic_id):
 
 def ids(problem):
   i = 0
+  total_expanded = 0
   while True:
     q = LimitedDepthQueue(i)
-    node = general_search(problem, q)
+    node, expanded = general_search(problem, q)
+    total_expanded += expanded
     if node != None:
-      return node
+      return node, total_expanded
     i += 1
 
 
@@ -57,4 +63,5 @@ class GreedyQueue(queue.PriorityQueue):
 if __name__ == "__main__":
   import ttfe, sys
   p = ttfe.TTFE(int(sys.argv[1]))
-  print(greedy(p))
+  solution, expanded = greedy(p, 0)
+  print(solution)
